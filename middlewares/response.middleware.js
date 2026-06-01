284912dcaac77e6f019948046d64195a0ebdb7f8
Volodymyr Suskyi <suskyivolodymyr@gmail.com>
2026-06-01 23:05:53 +0300

    implement responseMiddleware with 200/400/404 response handling

--- middlewares/response.middleware.js
+++ middlewares/response.middleware.js
@@ -1,6 +1,9 @@
 const responseMiddleware = (req, res, next) => {
-  // TODO: Implement middleware that returns result of the query
-  next();
+  if (res.err) {
+    const status = res.err.message?.toLowerCase().includes("not found") ? 404 : 400;
+    return res.status(status).json({ error: true, message: res.err.message });
+  }
+  res.status(200).json(res.data);
 };
 
 export { responseMiddleware };
