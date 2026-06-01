45d71f7800d04aa6c45b9375942a42a4bdfabace
Volodymyr Suskyi <suskyivolodymyr@gmail.com>
2026-06-01 23:13:11 +0300

    implement login route controller

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -8,8 +8,7 @@ router.post(
   "/login",
   (req, res, next) => {
     try {
-      // TODO: Implement login action (get the user if it exist with entered credentials)
-      res.data = data;
+      res.data = authService.login(req.body);
     } catch (err) {
       res.err = err;
     } finally {
