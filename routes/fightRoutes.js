3457ba71306c279fe5dcd7e497d74f2804ae7fb3
Volodymyr Suskyi <suskyivolodymyr@gmail.com>
2026-06-01 23:20:15 +0300

    implement fight simulation service and routes (POST /api/fights, GET /api/fights)

--- routes/fightRoutes.js
+++ routes/fightRoutes.js
@@ -1,13 +1,29 @@
 import { Router } from "express";
-import { fightersService } from "../services/fightService.js";
-import {
-  createUserValid,
-  updateUserValid,
-} from "../middlewares/user.validation.middleware.js";
+import { fightService } from "../services/fightService.js";
 import { responseMiddleware } from "../middlewares/response.middleware.js";
 
 const router = Router();
 
-// OPTIONAL TODO: Implement route controller for fights
+router.get("/", (req, res, next) => {
+  try {
+    res.data = fightService.getAll();
+  } catch (err) {
+    res.err = err;
+  } finally {
+    next();
+  }
+}, responseMiddleware);
+
+router.post("/", (req, res, next) => {
+  try {
+    const { fighter1, fighter2 } = req.body;
+    if (!fighter1 || !fighter2) throw new Error("fighter1 and fighter2 ids are required");
+    res.data = fightService.start(fighter1, fighter2);
+  } catch (err) {
+    res.err = err;
+  } finally {
+    next();
+  }
+}, responseMiddleware);
 
 export { router };
--- services/fightService.js
+++ services/fightService.js
@@ -1,9 +1,44 @@
 import { fightRepository } from "../repositories/fightRepository.js";
+import { fighterRepository } from "../repositories/fighterRepository.js";
 
-class FightersService {
-  // OPTIONAL TODO: Implement methods to work with fights
+function simulate(fighter1, fighter2) {
+  let health1 = fighter1.health;
+  let health2 = fighter2.health;
+  const log = [];
+
+  while (health1 > 0 && health2 > 0) {
+    const fighter1Shot = Math.max(1, fighter1.power - fighter2.defense);
+    const fighter2Shot = Math.max(1, fighter2.power - fighter1.defense);
+
+    health1 = Math.max(0, health1 - fighter2Shot);
+    health2 = Math.max(0, health2 - fighter1Shot);
+
+    log.push({ fighter1Shot, fighter2Shot, fighter1Health: health1, fighter2Health: health2 });
+  }
+
+  const winner = health1 > health2 ? fighter1.id : health2 > health1 ? fighter2.id : null;
+  return { log, winner };
 }
 
-const fightersService = new FightersService();
+class FightService {
+  getAll() {
+    return fightRepository.getAll();
+  }
 
-export { fightersService };
+  start(fighter1Id, fighter2Id) {
+    const fighter1 = fighterRepository.getOne({ id: fighter1Id });
+    if (!fighter1) throw new Error("Fighter 1 not found");
+
+    const fighter2 = fighterRepository.getOne({ id: fighter2Id });
+    if (!fighter2) throw new Error("Fighter 2 not found");
+
+    if (fighter1Id === fighter2Id) throw new Error("A fighter cannot fight themselves");
+
+    const { log, winner } = simulate(fighter1, fighter2);
+    return fightRepository.create({ fighter1: fighter1Id, fighter2: fighter2Id, winner, log });
+  }
+}
+
+const fightService = new FightService();
+
+export { fightService };
